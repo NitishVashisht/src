a3536172c61671ba2066e8f749043d424bbd4627
Nitish <vashishtnvashisht@icloud.com>
2018-02-05 20:41:16 +0530

    implementation of structural directives

--- app/directives/implementation.component.ts
+++ app/directives/implementation.component.ts
@@ -10,5 +10,10 @@ export class implementationComponent{
     truthy:Boolean = true;
     falsy:Boolean = false;
     view = "map";
+    subjects = [
+    {name:'maths',marks:95,percentage:'95%'},
+    {name:'sst',marks:85,percentage:'85%'},
+    {name:'sci',marks:90,percentage:'90%'}
+    ]
 }
 
