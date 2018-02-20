e2db4458b4430ef530bff43fe924c0f11fb4bafd
Nitish <vashishtnvashisht@icloud.com>
2018-02-20 15:55:27 +0530

    validations in template driven forms
    added validation in template driven forms

--- app/contact-form/contact-form.component.ts
+++ app/contact-form/contact-form.component.ts
@@ -6,6 +6,14 @@ it is also a type of template driven approach (using ng Model directive)to impos
 ng model with name implements this functionality by creating control object under the hood for input field
 so no need of using class= "form-control" in input tag.
 
+<input required ngModel name= "firstName" type="text" id = "firstName" #name= "ngModel" (change)= "log(name)">
+here "required" puts a validation means this field is required if no value is provided to input field then 
+we can raise an error of our choice just below this input field as 
+
+<div *ngIf="!name.valid && name.touched" class="alert alert-danger">this field is requied</div>
+
+ngIf is structural directive name is our template variable and .valid and .touhed are properties of control object that
+has created by angular under the hood and that we reference through our template variable.
 */
 
 import { Component, OnInit } from '@angular/core';
